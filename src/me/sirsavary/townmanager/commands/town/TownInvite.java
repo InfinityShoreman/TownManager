1c3150cda33406bfa1472e334c6119b6a3d7a381
SirSavary <jesse.savary@live.com>
2012-06-07 14:29:05 -0400

    Added debug code

--- src/me/sirsavary/townmanager/commands/town/TownInvite.java
+++ src/me/sirsavary/townmanager/commands/town/TownInvite.java
@@ -1,6 +1,7 @@
 package me.sirsavary.townmanager.commands.town;
 
 import me.sirsavary.townmanager.Chatter;
+import me.sirsavary.townmanager.Debug;
 import me.sirsavary.townmanager.IOManager;
 import me.sirsavary.townmanager.Main;
 import me.sirsavary.townmanager.commands.AbstractCommand;
@@ -28,21 +29,26 @@ public class TownInvite extends AbstractCommand {
 				if (townToJoin.getMayor().equalsIgnoreCase(sender.getName())) { //Invitee is the mayor of the town
 					//Send the player an invitation in the form of a question
 					invitedPlayer.sendMessage(Chatter.Message("The mayor of " + townToJoin.getColor() + townToJoin.getID() + Main.messageColor + " has invited you to join their town!"));
+					Debug.info(sender.getName() + " has invited " + invitedPlayer.getName() + " to join " + townToJoin.getID());
 					if (Main.questioner.ask(invitedPlayer, Chatter.Message("Would you like to join?"), Chatter.Message("yes"), Chatter.Message("no")).equals("yes")) { //If they accept it, add them to the town
 						invitedPlayer.sendMessage(Chatter.Message("You have accepted " + townToJoin.getColor() + townToJoin.getMayor() + "'s" + Main.messageColor + " invitation!"));
 						sender.sendMessage(Chatter.Message(invitedPlayer.getName() + " has joined " + townToJoin.getColor() + townToJoin.getID() + Main.messageColor + "!"));
 						IOManager.AddCitizen(invitedPlayer.getName(), townToJoin);
+						Debug.info(invitedPlayer.getName() + " accepted " + sender.getName() + "'s request to join" + townToJoin.getID());
 					}
 					else { //If they do not accept it, tell the mayor they're a fag
 						invitedPlayer.sendMessage(Chatter.Message("You have denied " + townToJoin.getColor() + townToJoin.getMayor() + "'s" + Main.messageColor + " invitation"));
+						Debug.info(invitedPlayer.getName() + " denied " + sender.getName() + "'s request to join" + townToJoin.getID());
 					}
 				}
 				else { //Invitee is not the mayor of the town
 					sender.sendMessage(Chatter.Message("You are not the mayor of " + townToJoin.getColor() + townToJoin.getID() + Main.messageColor + "!"));
+					Debug.warning(sender.getName() + " attempted to invite " + invitedPlayer.getName() + " to join " + townToJoin.getID() + " but they are not they mayor");
 				}
 			}
 			else { //Invited player is already part of a town
 				sender.sendMessage(Chatter.Message(invitedPlayer.getName() + " is already a part of a town!"));
+				Debug.warning(sender.getName() + " attempted to invite " + invitedPlayer.getName() + " to join " + townToJoin.getID() + " but they are already part of a town");
 			}
 		}
 		else { //Invited player does not exist
