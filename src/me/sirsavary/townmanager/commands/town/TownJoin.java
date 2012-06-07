8debb3d7adc6bfbcc100f72091be068e43c17f77
SirSavary <jesse.savary@live.com>
2012-06-07 14:34:38 -0400

    Added debug code

--- src/me/sirsavary/townmanager/commands/town/TownJoin.java
+++ src/me/sirsavary/townmanager/commands/town/TownJoin.java
@@ -1,6 +1,7 @@
 package me.sirsavary.townmanager.commands.town;
 
 import me.sirsavary.townmanager.Chatter;
+import me.sirsavary.townmanager.Debug;
 import me.sirsavary.townmanager.IOManager;
 import me.sirsavary.townmanager.Main;
 import me.sirsavary.townmanager.commands.AbstractCommand;
@@ -14,11 +15,13 @@ public class TownJoin extends AbstractCommand {
 
 	private final Town townToJoin;
 	Town t = IOManager.getPlayerTown((Player) sender);
+	String townName = null;
 
 	public TownJoin(CommandSender sender, boolean async, Main plugin, String TownToJoin)
 			throws Exception {
 		super(sender, async, plugin);
 		townToJoin = IOManager.getTown(TownToJoin);
+		townName = TownToJoin;
 	}
 
 	@Override
@@ -32,20 +35,25 @@ public class TownJoin extends AbstractCommand {
 					IOManager.AddCitizen(sender.getName(), townToJoin);
 					Main.server.getPlayer(townToJoin.getMayor()).sendMessage(Chatter.Message(sender.getName() + " has joined " + townToJoin.getFormattedID() + "!"));
 					sender.sendMessage(Chatter.TagMessage("You have joined " + townToJoin.getFormattedID() + "!"));
+					Debug.info(sender.getName() + " join " + townToJoin.getID());
 				}
 				else if (ht == HousingType.INVITE_ONLY) {
 					sender.sendMessage(Chatter.TagMessage(townToJoin.getFormattedID() + " is an invite only town!"));
+					Debug.warning(sender.getName() + " tried to join " + townToJoin.getID() + " but it has HousingType.INVITE_ONLY");
 				}
 				else if (ht == HousingType.CLOSED) {
 					sender.sendMessage(Chatter.TagMessage(townToJoin.getFormattedID() + " does not allow players to join!"));
+					Debug.warning(sender.getName() + " tried to join " + townToJoin.getID() + " but it has HousingType.CLOSED");
 				}
 			}
 			else {
 				sender.sendMessage(Chatter.TagMessage("That town does not exist!"));
+				Debug.severe(sender.getName() + " tried to join " + townName + " but it does not exist");
 			}
 		}
 		else {
 			sender.sendMessage(Chatter.TagMessage("You are already part of a town!"));
+			Debug.warning(sender.getName() + " tried to join " + townToJoin.getID() + " but they are already part of a town");
 		}
 	}
 
